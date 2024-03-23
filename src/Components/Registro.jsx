6daa89b03e02e47167641230987b4e7f0fdfaf08
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-03-22 18:59:50 -0600

    creando componente registro usuarios

--- src/Components/Registro.jsx
+++ src/Components/Registro.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export const Registro = () => {
 
-    const [Form, setForm] = useState({ nombre: '', gmail: '', imagen: '', contrasenia: ''})
+    const [Form, setForm] = useState({ nombre: '', gmail: '', imagen: '', contrasenia: '' })
     const [mensaje, setMensaje] = useState('')
 
     const onChangeHandler = (e) => {
-        const {name, value} = e.target;
-        
+        const { name, value } = e.target;
+
         if (name === "imagen") {
             const img = e.target.files[0];
             setForm({ ...Form, [name]: img });
@@ -18,7 +18,13 @@ export const Registro = () => {
     }
 
     const submitHandler = async (event) => {
-        try{
+        try {
+            if (Form.nombre.length === 0 || Form.gmail.length === 0 ||
+                Form.imagen.length === 0 || Form.contrasenia.length === 0) {
+                setMensaje("completa los campos")
+                return
+            }
+
             event.preventDefault();
             const url = "http://localhost:5000/api/usuario"
             const dataFormulario = new FormData()
@@ -29,39 +35,45 @@ export const Registro = () => {
             dataFormulario.append("contrasenia", Form.contrasenia)
             await axios.post(url, dataFormulario)
             setMensaje("Usuario Registrado!")
-        }catch(err){
+        } catch (err) {
             console.log(err)
+            setMensaje("ocurrio un error!")
         }
     }
 
-  return (
-    <>
-      <div className='container'>
-        <div className="row mx-auto">
-            <div className="col-5 mx-auto mt-5">
-                      <form  onSubmit={submitHandler}>
-                    <div className="mb-3">
-                        <label htmlFor='nombre' className="form-label">Nombre Usuario</label>
-                              <input type="text" className="form-control" name='nombre' onChange={onChangeHandler} aria-describedby="emailHelp"/>
-                    </div>
-                        <div className="mb-3">
-                            <label htmlFor="exampleInputPassword1" className="form-label">Correo electronico</label>
-                              <input type="email" name='gmail' onChange={onChangeHandler} className="form-control" />
-                        </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Contrasena</label>
-                              <input type="password" name='contrasenia' onChange={onChangeHandler} className="form-control" />
-                    </div>
-                        <div className='mb-3'>
-                            <label htmlFor="formFileLg" className="form-label">Foto de perfil</label>
-                              <input className="form-control form-control-lg" onChange={onChangeHandler} name='imagen' type="file"/>
-                        </div>
-                    <button type="submit" className="btn btn-primary w-50">Registrar</button>
-                </form>
-            </div>
-        </div>
-      </div>
+    return (
+        <>
+            <div className='container'>
+                <div className="row mx-auto">
+                    <div className="col-5 mx-auto mt-5">
+                        <form onSubmit={submitHandler}>
+                            <div className="mb-3">
+                                <label htmlFor='nombre' className="form-label">Nombre Usuario</label>
+                                <input type="text" className="form-control" name='nombre' onChange={onChangeHandler} aria-describedby="emailHelp" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">Correo electronico</label>
+                                <input type="email" name='gmail' onChange={onChangeHandler} className="form-control" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">Contrasena</label>
+                                <input type="password" name='contrasenia' onChange={onChangeHandler} className="form-control" />
+                            </div>
+                            <div className='mb-3'>
+                                <label htmlFor="formFileLg" className="form-label">Foto de perfil</label>
+                                <input className="form-control form-control-lg" onChange={onChangeHandler} name='imagen' type="file" />
+                            </div>
 
-    </>
-  )
+                        </form>
+                        <div class="d-grid gap-2 d-md-flex">
+                            <button type="button" className="btn btn-primary w-50" onClick={submitHandler}>Registrar</button>
+                            <button type="button" className="btn btn-primary w-50">Limpiar</button>
+                        </div>
+                        <div className='mensaje'>{mensaje}</div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
